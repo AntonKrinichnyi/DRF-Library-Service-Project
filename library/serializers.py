2203e077f1b5638658d928fe5e306908863fa375
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-22 19:05:32 +0200

    add author serializer

---
+++ library/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from library.models import Author, Book, Borrowing, Payment
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ("id", "first_name", "last_name")
+
+
