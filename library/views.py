d90d34ddc3b4b6ba5d551365b630844ecaa0919c
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-22 19:31:22 +0200

    create Author and book viewsets

--- library/views.py
+++ library/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from library.models import Author, Book, Borrowing, Payment
+from library.serializers import (AuthorSerializer,
+                                 BookSerializer,
+                                 BorrowingSerializer,
+                                 Payment)
 
-# Create your views here.
+
+class AuthorViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
