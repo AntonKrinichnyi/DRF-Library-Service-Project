08fcb05ae170606d66dc0f54c1a065ec841564de
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-22 17:44:00 +0200

    create book model

--- library/models.py
+++ library/models.py
@@ -9,4 +9,15 @@ class Author(models.Model):
         return self.first_name + "-" + self.last_name
 
 class Book(models.Model):
+    class CoverChoises(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+        
     title = models.CharField(max_length=255, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    cover = models.CharField(max_length=10, choices=CoverChoises)
+    inventory = models.IntegerField(min=0)
+    daily_fee = models.DecimalField(max_digits=2, min=0)
+    
+    def __str__(self):
+        return self.title
