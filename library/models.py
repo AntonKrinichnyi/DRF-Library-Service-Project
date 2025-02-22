881ddc0b477d9d5581f292cdcf8a59b16f6fbd95
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-22 17:31:30 +0200

    create author model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=155)
+    last_name = models.CharField(max_length=155)
+    
+    def __str__(self):
+        return self.first_name + "-" + self.last_name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True)
