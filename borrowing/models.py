00b8e6ef42ee2d803de523c322f4be513515b56d
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-24 12:50:25 +0200

    starting restructurate project and create single app for borrowing payment and book

---
+++ borrowing/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from library_service import settings
+from book.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateTimeField(auto_now_add=True,
+                                              blank=True,
+                                              null=True)
+    book = models.ManyToManyField(Book)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return f"{self.user.email} borrow by {self.borrow_date}"
