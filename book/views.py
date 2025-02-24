33a26bdaa7ddf19e31b7138c13cd963a3add220b
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-24 20:13:10 +0200

    add view for book app

--- book/views.py
+++ book/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from book.models import Author, Book
+from book.serializers import AuthorSerializer, BookListSerializer, BookSerializer
 
-# Create your views here.
+
+class AuthorViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().prefetch_related("authors")
+    serializer_class = BookSerializer
+    
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BookListSerializer
+        return BookSerializer
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer
 
-# Create your views here.
+
+class BorrowingViewSet(mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.CreateModelMixin,
+                       viewsets.GenericViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
