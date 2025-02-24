126f9475001af99804357a7197a1632418a1b36d
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-24 21:13:20 +0200

    add task for borrowing to send overdue borrow messeges

--- telegram_notificated.py
+++ telegram_notificated.py
@@ -5,7 +5,7 @@ import requests
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def telegram_notificated(message):
+def send_telegram_notification(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
     response = requests.post(url, data=data)
