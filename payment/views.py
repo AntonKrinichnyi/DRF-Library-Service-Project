26daa4fb23af2059b6b6b9acd8ea6dd2cbb01e34
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-26 10:48:35 +0200

    take a permissions for payment view set

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from payment.models import Payment
-from payment.serializers import PaymentSerializer
+from payment.serializers import PaymentDetailSerializer, PaymentSerializer
 
 
-class PaymentViewSet(viewsets.ModelViewSet):
+class PaymentViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     queryset = Payment.objects.all().select_related("borrowing_id")
     serializer_class = PaymentSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentSerializer
+    
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [IsAuthenticated()]
+        return [IsAdminUser()]
