254d77b02338e34139f2f7873106e9e91c6e3fb0
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-26 12:02:25 +0200

    fix urls for payment and user apps

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
-from payment.views import PaymentViewSet
+from payment.views import (PaymentViewSet,
+                           payment_succes,
+                           payment_cancel)
 
 router = routers.DefaultRouter()
 router.register("payment", PaymentViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("payment_succes/<str:session_id>/",
+         payment_succes,
+         name="payment_succes"),
+    path("payment_cancel/", payment_cancel, name="payment_cancel"),
+]
 
 app_name = "payment"
